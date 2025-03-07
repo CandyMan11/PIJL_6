070b9755cc623e26da0ebae595d7e7c2a818bf0d
Saksham Sharma <sakshamsharma1104@gmail.com>
2025-03-07 21:37:10 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+package Assignment_6;
+
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        Duck[] ducks = {new MallardDuck(), new RedheadDuck(), new RubberDuck(), new DecoyDuck()};
+        
+        for (Duck duck : ducks){
+            duck.display();
+            duck.performFly();
+            duck.performSwim();
+            System.out.println();
+        }
+    }    
+}
